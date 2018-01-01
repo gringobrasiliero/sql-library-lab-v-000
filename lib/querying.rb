0d09ca9370d5b6255ba28df408b4322bb05e86f2
Nolan Byrnes <nolanehs@gmail.com>
2018-01-01 23:01:13 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -14,7 +14,7 @@ end
 
 
 def select_value_and_count_of_most_prolific_species
-  "SELECT characters, COUNT(*)
+  "SELECT species, COUNT(*)
   FROM characters
   GROUP BY species
   ORDER BY species DESC LIMIT 1 "
