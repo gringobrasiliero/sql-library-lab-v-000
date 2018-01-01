3f39c43afe33f7715fc4efce3c784437e1f1abcc
Nolan Byrnes <nolanehs@gmail.com>
2018-01-01 23:18:22 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -21,7 +21,10 @@ def select_value_and_count_of_most_prolific_species
 end
 
 def select_name_and_series_subgenres_of_authors
-  "Write your SQL query here"
+  "SELECT authors.name, subgenres.name
+   FROM subgenres
+   JOIN authors ON authors.id = series.author_id
+   JOIN series ON series.subgenre_id = subgenres.id";
 end
 
 def select_series_title_with_most_human_characters
